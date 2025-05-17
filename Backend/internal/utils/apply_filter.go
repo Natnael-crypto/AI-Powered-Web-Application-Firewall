0d665ffd437608eb0b44a0fddbd28b563cf55cac
Natnael-crypto <yohannesnatnael9@gmail.com>
2025-05-17 20:12:23 +0300

    updated Backend

--- Backend/internal/utils/apply_filter.go
+++ Backend/internal/utils/apply_filter.go
@@ -48,7 +48,7 @@ func ApplyRequestFilters(c *gin.Context) *gorm.DB {
 		query = query.Where("body ILIKE ?", "%"+body+"%")
 	}
 	if response_code := c.Query("response_code"); response_code != "" {
-		query = query.Where("response_code = ?", "%"+response_code+"%")
+		query = query.Where("response_code = ?", response_code)
 	}
 	if rule_detected := c.Query("rule_detected"); rule_detected != "" {
 		query = query.Where("rule_detected ILIKE ?", "%"+rule_detected+"%")
