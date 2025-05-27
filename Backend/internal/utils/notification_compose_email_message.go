0f93a736784ac97b7f993fd9f1ec103a1a561a93
Natnael-crypto <yohannesnatnael9@gmail.com>
2025-05-27 12:44:58 +0300

    moved the email composer function to utils

---
+++ Backend/internal/utils/notification_compose_email_message.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+func ComposeEmailMessage(username, ruleName, hostName string, clientIPs []string, count int64, startTimestamp int64) string {
+	seconds := startTimestamp / 1000
+	nanoseconds := (startTimestamp % 1000) * int64(time.Millisecond)
+	startTime := time.Unix(seconds, nanoseconds).Format("2006-01-02 15:04:05 MST")
+
+	endTime := time.Now().Format("2006-01-02 15:04:05 MST")
+
+	return fmt.Sprintf(
+		`Hello %s,
+
+Your Web Application Firewall has detected suspicious activity.
+
+🛡️ Rule Triggered: %s
+📌 Application: %s
+🌐 Source IP(s): %s
+🔢 Occurrence Count: %d
+🕒 Time: %s to %s
+
+Recommended Action: Please review the related logs and ensure appropriate mitigation steps are in place.
+
+Best regards,
+Gasha WAF Security Monitoring System`,
+		username,
+		ruleName,
+		hostName,
+		clientIPs,
+		count,
+		startTime,
+		endTime,
+	)
+}
