876d0075bac34e28eb5285d760796a254cb926bb
Natnael-crypto <yohannesnatnael9@gmail.com>
2025-06-19 08:47:22 +0300

    domain name

--- Backend/internal/controllers/application_controller.go
+++ Backend/internal/controllers/application_controller.go
@@ -23,7 +23,7 @@ func AddApplication(c *gin.Context) {
 		ApplicationName string `json:"application_name" binding:"required,max=20"`
 		Description     string `json:"description" binding:"required,max=200"`
 		HostName        string `json:"hostname" binding:"required,max=40"`
-		IpAddress       string `json:"ip_address" binding:"required,ip"`
+		IpAddress       string `json:"ip_address" binding:"required"`
 		Port            string `json:"port" binding:"required,max=5"`
 		Status          *bool  `json:"status"`
 		Tls             *bool  `json:"tls"`
@@ -216,7 +216,7 @@ func UpdateApplication(c *gin.Context) {
 		ApplicationName string `json:"application_name" binding:"required,max=20"`
 		Description     string `json:"description" binding:"required,max=200"`
 		HostName        string `json:"hostname" binding:"required,max=40"`
-		IpAddress       string `json:"ip_address" binding:"required,ip"`
+		IpAddress       string `json:"ip_address" binding:"required"`
 		Port            string `json:"port" binding:"required,max=5"`
 		Status          *bool  `json:"status" binding:"required"`
 		Tls             *bool  `json:"tls" binding:"required"`
